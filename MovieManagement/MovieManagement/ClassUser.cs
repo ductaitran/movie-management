636f1dc5ddd41558926a18f3de823eff91423bc9
johncegom <johncegom203@gmail.com>
2020-05-01 15:15:35 +0700

    Add deleteUser function in User tab

--- MovieManagement/MovieManagement/ClassUser.cs
+++ MovieManagement/MovieManagement/ClassUser.cs
@@ -33,5 +33,32 @@ namespace MovieManagement
                 MessageBox.Show(ex.Message);
             }
         }
+
+        public static void DeleteUser(ref DataGridView dataGridViewUser)
+        {
+            try
+            {
+                string id = dataGridViewUser.SelectedCells[0].Value.ToString();
+                DialogResult deleted = MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (deleted == DialogResult.Yes)
+                {
+                    string query = @"DELETE FROM Users WHERE users_id = '" + id + "'";
+
+                    clsConnection.openConnection();
+                    SqlCommand cmd = new SqlCommand(query, clsConnection.con);
+                    cmd.ExecuteNonQuery();
+
+                    clsConnection.closeConnection();
+                    MessageBox.Show("This user has been deleted!");
+                    loadDataGridViewUser(ref dataGridViewUser);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
